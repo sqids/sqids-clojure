5a7f64e115ed57c4cb648c8774481b2a5027233a
Łukasz Korecki <lukasz@korecki.me>
2023-11-27 16:29:26 -0800

    More tests, LSP config

--- Makefile
+++ Makefile
@@ -1,8 +1,11 @@
-.PHON: get-jar test
+.PHONY: get-jar test fmt
+
 get-jar:
 	mkdir -p vendor
 	curl 'https://s01.oss.sonatype.org/content/repositories/snapshots/org/sqids/sqids/0.1.0-SNAPSHOT/sqids-0.1.0-20231127.135843-1.jar' -o vendor/sqids.jar
 
+fmt:
+	clojure-lsp format
 
 test:
 	clj -M:test
--- test/sqids_test.clj
+++ test/sqids_test.clj
@@ -1,6 +1,6 @@
 (ns sqids-test
   (:require
-   [clojure.test :refer [deftest is testing use-fixtures]]
+   [clojure.test :refer [deftest is testing]]
    [sqids :as sqids]))
 
 (deftest roundtrip-test
@@ -9,10 +9,17 @@
 (deftest string-reprentation-test
   (is (= "86Rf07" (sqids/encode [1 2 3]))))
 
-(deftest with-new-engine
+(deftest with-custom-engine
   (let [engine (sqids/build {:min-length 16 :alphabet "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"})
         input [1 2 3 8 9 10 11 12 13]
         encoded (sqids/encode engine input)]
     (is (= "sLiMkurjEvaLbbFqso" encoded))
     (is (= 18 (count encoded)))
-    (is (= input (sqids/decode engine encoded)))))
+
+    (testing "roundtrip"
+      (is (= input (sqids/decode engine encoded))))
+
+    (testing "output will be different because alphabets are different"
+      (let [encoded-og (sqids/encode input)]
+        (is (= "147sLyt0n25Wk0BQxe" encoded-og))
+        (is (not= encoded encoded-og))))))
